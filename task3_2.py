99709562cf8bb3f266b8753eb52ff92f66d3624a
Michał Baran <baran.michal89@gmail.com>
2025-01-05 15:39:42 +0100

    Add code in polish

--- task3_2.py
+++ task3_2.py
@@ -26,31 +26,4 @@ for sklep,rzeczy in listaZakupow.items():
     sklep=sklep.capitalize()
     rzeczy=[produkt.capitalize() for produkt in rzeczy]
     print(f"Idę do {sklep} i kupuję tam {rzeczy}")
-
-
-# Zadanie 2
-# Napisz program, który:
-
-# Dla liczb z zakresu od 0 do 100, wyświetli te, które są podzielne przez 5.
-# W następnym wierszu wyświetli te liczby podniesione do potęgi 3.
-# Przesyłanie zadań
-# Zrób zrzuty ekranu z uruchomienia Twoich programów przez VSCode wraz z ich kodem źródłowym i wyślij je swojemu Mentorowi.
-
-# Wskazówka: Staraj się trzymać porządek w strukturze projektu i plików. Dobrym pomysłem będzie utrzymywanie każdego zadania jako osobnego pliku.
-
-# Zestaw zrzutów ekranu przekaż Mentorowi w jeden z poniższych sposobów:
-
-# umieść je w nowym katalogu na swoim dysku sieciowym (np. Dropbox, Google Drive, etc.), wklej poniżej link do katalogu (uzyskany za pomocą guzika "Udostępnij"),
-# otwórz nowy dokument na Google Docs i przeciągnij do niego zrzuty ekranu, wklej poniżej link do tego dokumentu (uzyskany za pomocą guzika "Udostępnij"),
-# zrób zdjęcie ekranu aparatem typu Polaroid i wyślij je gołębiem pocztowym.
-num=0
-liczby=[]
-while num<101:
-    liczby=liczby+[num]
-    num=num+1
-
-liczbyPodzielnePrzez5=[liczba for liczba in liczby if liczba%5==0]
-liczbyPodzielnePrzez5DoPotegi3=[liczba**3 for liczba in liczbyPodzielnePrzez5]
-print(liczby)
-print(liczbyPodzielnePrzez5)
-print(liczbyPodzielnePrzez5DoPotegi3)
+    
